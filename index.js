88373335f71b6dae4dc433410fc52bbde64bdbe1
Mang <mangemmanuel07@gmail.com>
2023-03-22 05:46:48 +0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.querySelector('main#main'); {
+main.remove()
+}
+
+const newHeader = document.createElement('h1'); {
+    newHeader.id = ('victory')
+}
+
+const myName = 'Mang Emmanuel'; 
+    newHeader.textContent = '${myName} is the champion.'
+
+
